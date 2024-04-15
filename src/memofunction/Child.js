544bd2386e4b4a25c8f35830c29080051abeeb32
Ayesha Nizam <ayeshanizam39@gmail.com>
2024-04-15 18:08:47 +0500

    usememo

--- src/memofunction/Child.js
+++ src/memofunction/Child.js
@@ -1,13 +1,19 @@
-import React,{memo} from 'react';
+import React from 'react';
 import Parent from './Parent';
-const Child = memo(({onIncrement}) => {
+const Child=()=>{
+  console.log("child component rendered")
+
+// const Child = memo(({todos, addTodo}) => {
     // console.log("child component Rendered")
   return (
     <div>
-      <h1>Child Component</h1>
-      <button onClick={onIncrement}>Increment Count</button>
+      {/* <h1>Child Component</h1>
+      <button onClick={onIncrement}>Increment Count</button> */}
+      
+      <div>
+      <h1>Hello World</h1>
+    </div>
     </div>
   );
-})
-
+}
 export default Child;
--- src/memofunction/Parent.js
+++ src/memofunction/Parent.js
@@ -1,18 +1,30 @@
 import React,{useState,useCallback} from 'react';
 import Child from './Child';
 const Parent = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  // Memoize the increment function
-  const increment = useCallback(() => {
-    setCount(count + 1);
-  }, [count]); // Recreate the function only if count changes
-    
+  // // Memoize the increment function
+  // const increment = useCallback(() => {
+  //   setCount(count + 1);
+  // }, [count]); // Recreate the function only if count changes
+   const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const [num, setNum] = useState(0);
+  // const increment = () => {
+  //   setCount((c) => c + 1);
+  //   };
+  // const addTodo = useCallback(() => {
+  //   setTodos((t) => [...t, "New Todo"]);
+  // }, [todos]);
+
   return (
     <div>
-     <p>Count: {count}</p>
-      <Child onIncrement={increment}/>
- 
+     {/* <p>Count: {count}</p>
+      <Child onIncrement={increment}/> */}
+      {/* <Child todos={todos} addTodo={addTodo} /> */}
+      <h1>{num}</h1>
+      <Child />
+      <button onClick={() => setNum(num + 1)}> Addition </button>
     </div>
   );
 
